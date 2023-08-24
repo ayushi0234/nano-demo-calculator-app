276f2d508c65fb57bd26ea312d95bb3c45dc1cb7
ayushi0234 <92812265+ayushi0234@users.noreply.github.com>
2023-08-24 11:24:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,19 +10,21 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.status(200).send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    res.status(200).json({ "result": first + second });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+    res.status(200).json({ "result": first - second });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+    console.log("Server running at PORT", PORT);
 });
